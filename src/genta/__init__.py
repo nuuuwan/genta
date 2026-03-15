eee19dc7885abed4658e091650b25535393b5064
nuuuwan <nuuuwan@gmail.com>
2026-03-15 08:34:24 -0700

    Remove deprecated assistant classes and update requirements; add pipeline script

--- src/genta/__init__.py
+++
@@ -1,5 +0,0 @@
-# genta (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from genta.AbstractAssistant import AbstractAssistant
-from genta.custom import AdaAssistant, DaynaAssistant, TaraAssistant
--- src/genta/custom/__init__.py
+++
@@ -1,6 +0,0 @@
-# genta.custom (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from genta.custom.AdaAssistant import AdaAssistant
-from genta.custom.DaynaAssistant import DaynaAssistant
-from genta.custom.TaraAssistant import TaraAssistant
